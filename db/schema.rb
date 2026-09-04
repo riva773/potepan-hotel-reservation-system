4c85a25ebfac7d064a1e70f5a66fe2443c555e10
riva773 <teslaicaer@gmail.com>
2026-09-04 14:29:43 +0900

    ER図の作成

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_09_03_063428) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "remember_crea∏ted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name", null: false
