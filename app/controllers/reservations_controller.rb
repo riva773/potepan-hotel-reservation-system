9632f582d995b88954367c3392c4ffa38a7f47ed
riva773 <teslaicaer@gmail.com>
2026-09-06 17:04:05 +0900

    予約更新機能の実装

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -15,24 +15,41 @@ class ReservationsController < ApplicationController
   end
 
   def edit
+    @reservation = Reservation.find(params[:id])
   end
 
   def update
+    @reservation = Reservation.find(params[:reservation][:id])
+    if @reservation.update(reservation_params)
+      redirect_to reservations_path, notice: "予約を更新しました。"
+    else
+      render :confirm, status: :unprocessable_entity
+    end
+
+
   end
 
   def destroy
   end
 
   def confirm
-    @reservation = Reservation.new(reservation_params)
-    @hotel = Hotel.find(reservation_params[:hotel_id])
-    @reservation.hotel = @hotel
+    if params[:reservation][:id]
+      @reservation = Reservation.find(params[:reservation][:id])
+      @reservation.check_in = params[:reservation][:check_in]
+      @reservation.check_out = params[:reservation][:check_out]
+      @reservation.attendance = params[:reservation][:attendance]
+      @hotel = @reservation.hotel
+    else
+      @reservation = Reservation.new(reservation_params)
+      @hotel = Hotel.find(reservation_params[:hotel_id])
+      @reservation.hotel = @hotel
+    end
     @duration = (@reservation.check_out - @reservation.check_in).to_i
     @sum_price = @hotel.price * @reservation.attendance * @duration
     render :confirm
   end
 
   def reservation_params
-    params.require(:reservation).permit(:check_in, :check_out, :attendance, :hotel_id, :sum_price)
+    params.require(:reservation).permit(:check_in, :check_out, :attendance, :hotel_id, :sum_price, :id)
   end
 end
